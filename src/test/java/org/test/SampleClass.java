01e69b3869d68185209174c7c0e6905254a73323
ganeeshkarthik <ganeshkarthik.gk25@gmail.com>
2021-04-27 18:05:46 +0530

    Git Sample

---
+++ src/test/java/org/test/SampleClass.java
@@ -0,0 +1,20 @@
+package org.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleClass {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+
+	}
+
+}
